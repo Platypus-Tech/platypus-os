4d4125b23a8927d6d1c5c0bbddbf36bff1f198a0
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-19 13:16:31 -0700

    Update main.c

--- kernel/main.c
+++ kernel/main.c
@@ -4,5 +4,9 @@
  
 void kernel_main(void) {
 	terminal_initialize();
-	terminal_writestring("Welcome to Platypus OS\n");
+	terminal_writestring("Welcome to Platypus OS!\n");
+	terminal_writestring("This is still in development, so there isn't anything to do...\n\n\n");
+	terminal_writestring("Yet.\n");
+	terminal_writestring("If you want to help, go to https://github.com/Platypus-Tech/new-platypus-os-drafts/ to help\n\n");
+	terminal_writestring("Or to see the source code!");
 }
