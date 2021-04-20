6e691366a550fc57ab96b34977b8f0afc2bd5d23
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-20 08:17:37 -0700

    I hope I put all the needed functions...

--- kernel/main.c
+++ kernel/main.c
@@ -3,11 +3,18 @@
 #include <kernel/tty.h>
  
 void kernel_main(void) {
-	terminal_initialize();
-	
-	terminal_writestring("Welcome to Platypus OS!\n");
-	terminal_writestring("This is still in development, so there isn't anything to do...\n\n\n");
-	terminal_writestring("Yet.\n");
-	terminal_writestring("If you want to help, go to https://github.com/Platypus-Tech/new-platypus-os-drafts/ to help\n\n");
-	terminal_writestring("Or to see the source code!");
+    terminal_initialize();
+    fptr = fopen("ascii-art.txt","r");
+    char c;
+    c = fgetc(fptr);
+    while (c != EOF){
+        printf("%c", c);
+        c = fgetc(fptr);
+    };
+    fclose(fptr);
+    terminal_writestring("Welcome to Platypus OS!\n");
+    terminal_writestring("This is still in development, so there isn't anything to do...\n\n\n");
+    terminal_writestring("Yet.\n");
+    terminal_writestring("If you want to help, go to https://github.com/Platypus-Tech/new-platypus-os-drafts/ to help\n\n");
+    terminal_writestring("Or to see the source code!");
 }
