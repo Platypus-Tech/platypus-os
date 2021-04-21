cca09dfd31ba0c2dfc4d6c9c7caaa6e658401247
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-21 11:53:23 -0700

    Update keyboard.c

--- kernel/devices/keyboard/keyboard.c
+++ kernel/devices/keyboard/keyboard.c
@@ -1,5 +1,5 @@
 /********************************************************************
- * Keyboard stuff! Yay! :D :D :D                                    *
+ * Keyboard stuff :D                                                *
  * (c) DamieFC                                                      *
  ********************************************************************/
 #include <stdio.h>
@@ -7,5 +7,19 @@
 #include "keyboard.h"
 
 int keyboard_driver(){
+    struct keyboard keyboard
+    keyboard.init init;
+    keyboard.work work;
+}
+
+void init(){
+    unsigned char keymap[128] = {
+        0,27,"1","2","3","4","5","6","7","8","9","0","-","+","\b",
+        "\t","q","w","e","r","t","y","u","i","o","p","[","]","\",
+        
+    }
+}
+
+void work(){
     
 }
