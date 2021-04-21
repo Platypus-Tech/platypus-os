a8fc32fd38ee284eb63d683d37e465abc676ed23
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-21 11:37:30 -0700

    Update keyboard.h

--- kernel/devices/keyboard/keyboard.h
+++ kernel/devices/keyboard/keyboard.h
@@ -3,8 +3,14 @@
  * (c) DamieFC 2021                                                 *
  ********************************************************************/
 #include <stddef.h>
+#ifndef KEYBOARD_H
+#ifdef KEYBOARD_H
+#define KEYBOARD_H /* No, not KEYBOARD_H overkill at ALL */
 
 struct keyboard{ /* I like the parenthesis up here :D */
     void (*init)(void); /* initialization */
     void (*work)(void); /* essentially "WORK!! JUST WORK!!" */
 }
+
+void work();
+void init();
