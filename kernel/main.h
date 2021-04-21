22bf9187d093aa19ecf11ddfeeaaa42f01b62910
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-21 09:50:07 -0700

    Heck if I know why

---
+++ kernel/main.h
@@ -0,0 +1,8 @@
+/************************************************************************
+ * Maybe it needs this function? Heck if I know.                        *
+ * (c) Copyleft 2021 DamieFC                                            *
+ ************************************************************************/
+
+#include <stddef.h>
+
+void kernel_main(void){}
