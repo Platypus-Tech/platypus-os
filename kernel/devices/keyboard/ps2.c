97a3e359ce7034e1f6b70163a880bec5f1ea66bd
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-21 15:28:04 -0700

    58 keys? And only esc, `, 1, and 2 work.
    dude. wtf

--- kernel/devices/keyboard/ps2.c
+++ kernel/devices/keyboard/ps2.c
@@ -4,6 +4,7 @@
  ********************************************************************/
 #include <stdio.h>
 #include <stddef.h>
+#include <log/log.h>
 #include "ps2.h"
 
 int ps2_keyboard_driver(){
@@ -13,15 +14,25 @@ int ps2_keyboard_driver(){
 }
 
 void init(){
-    unsigned char keymap[128] = {
-        0,27,"1","2","3","4","5","6","7","8","9","0","-","+","\b",
-        "\t","q","w","e","r","t","y","u","i","o","p","[","]","\\",
-        0,"a","s","d","f","g","h","j","k","l",";","'","\n",
-        0,"z","x","c","v","b","n","m",",",".","/",0,
-        0,0," ",0,0, /* OK that's not 128, I still need the keypad and stuff */
+    char keymap[58/*?*/] = {
+        0,"`","1","2","3","4","5","6","7","8","9","0","-","+","\b", /* 15 */
+        "\t","q","w","e","r","t","y","u","i","o","p","[","]","\\", /* 14 */
+        0,"a","s","d","f","g","h","j","k","l",";","'","\n", /* 13 */
+        0,"z","x","c","v","b","n","m",",",".","/",0, /* 12 */
+        0,0," ",0,0, /* 5 */    /* 15 + 14 + 13 + 12 + 5 is... 59? Then subtract one because it starts at 0, 58?*/
     }
+    log(INFO, "Keyboard initialized!");
 }
 
 void work(){
-    
+    switch (keymap){
+        case 0:
+            printf("");
+        case 1:
+            printf("`");
+        case 2:
+            printf("1");
+        case 3:
+            printf("2");
+    }
 }
