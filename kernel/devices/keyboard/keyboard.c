074d94d95ad79975fc8bf77cb7a0c2a18df9fe36
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-21 11:35:57 -0700

    Skeleton driver thing

---
+++ kernel/devices/keyboard/keyboard.c
@@ -0,0 +1,11 @@
+/********************************************************************
+ * Keyboard stuff! Yay! :D :D :D                                    *
+ * (c) DamieFC                                                      *
+ ********************************************************************/
+#include <stdio.h>
+#include <stddef.h>
+#include "keyboard.h"
+
+int keyboard_driver(){
+    
+}
