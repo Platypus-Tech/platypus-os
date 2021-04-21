d49bab2d53dca44a50e0c466b6a48bc948393132
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-21 11:28:30 -0700

    Workin' on header files for keyboard

---
+++ kernel/devices/keyboard/keyboard.h
@@ -0,0 +1,10 @@
+/********************************************************************
+ * Gonna do OOP for the keyboard. Just an idea.                     *
+ * (c) DamieFC 2021                                                 *
+ ********************************************************************/
+#include <stddef.h>
+
+struct keyboard{ /* I like the parenthesis up here :D */
+    void init(void); /* initialization */
+    void work(void); /* essentially "WORK!! JUST WORK!!" */
+}
