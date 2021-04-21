bfed584cbcf1c69211d1a1922211f9c7018f35dc
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-21 15:31:45 -0700

    No it's 59, make esc null, add more keys
    Now esc ` 1 2 3 4 5 6

--- kernel/devices/keyboard/ps2.c
+++ kernel/devices/keyboard/ps2.c
@@ -14,12 +14,12 @@ int ps2_keyboard_driver(){
 }
 
 void init(){
-    char keymap[58/*?*/] = {
-        0,"`","1","2","3","4","5","6","7","8","9","0","-","+","\b", /* 15 */
+    char keymap[59/*?*/] = {
+        "esc","`","1","2","3","4","5","6","7","8","9","0","-","+","\b", /* 15 */
         "\t","q","w","e","r","t","y","u","i","o","p","[","]","\\", /* 14 */
         0,"a","s","d","f","g","h","j","k","l",";","'","\n", /* 13 */
         0,"z","x","c","v","b","n","m",",",".","/",0, /* 12 */
-        0,0," ",0,0, /* 5 */    /* 15 + 14 + 13 + 12 + 5 is... 59? Then subtract one because it starts at 0, 58?*/
+        0,0," ",0,0, /* 5 */    /* 15 + 14 + 13 + 12 + 5 is... 59? Then subtract one because it starts at 0, 58? No, 59. AAAA I'll just do 59. */
     }
     log(INFO, "Keyboard initialized!");
 }
@@ -27,12 +27,22 @@ void init(){
 void work(){
     switch (keymap){
         case 0:
-            printf("");
+            null
         case 1:
             printf("`");
         case 2:
             printf("1");
         case 3:
             printf("2");
+        case 4:
+            printf("3");
+        case 5:
+            printf("4");
+        case 6:
+            printf("5");
+        case 7:
+            printf("6");
+        default:
+            null
     }
 }
