9e038351d296855c59fd6e608a67fd6fcb98ec01
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-22 11:11:19 -0700

    Update stdio.h

--- libc/include/stdio.h
+++ libc/include/stdio.h
@@ -1,14 +1,14 @@
 #ifndef _STDIO_H
 #define _STDIO_H 1
- 
+
 #include <sys/cdefs.h>
- 
+
 #define EOF (-1)
- 
+
 #ifdef __cplusplus
 extern "C" {
 #endif
- 
+
 int printf(const char* __restrict, ...);
 int putchar(int);
 int puts(const char*);
@@ -20,5 +20,5 @@ extern int fclose (FILE *__stream); /* fclose() */
 #ifdef __cplusplus
 }
 #endif
- 
+
 #endif
