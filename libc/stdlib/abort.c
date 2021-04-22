0b84e82ba8d94dcdd8db5a4ae90550442889595b
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-22 10:58:40 -0700

    Update abort.c

--- libc/stdlib/abort.c
+++ libc/stdlib/abort.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 __attribute__((__noreturn__))
 void abort(void) {
 #if defined(__is_libk)
-	// TODO: Add proper kernel panic.
-	printf("kernel: panic: abort()\n");
+    // TODO: Add proper kernel panic.
+    printf("kernel: panic: abort()\n");
 #else
-	// TODO: Abnormally terminate the process as if by SIGABRT.
-	printf("abort()\n");
+    // TODO: Abnormally terminate the process as if by SIGABRT.
+    printf("abort()\n");
 #endif
-	while (1) { }
-	__builtin_unreachable();
+    while (1) { }
+    __builtin_unreachable();
 }
