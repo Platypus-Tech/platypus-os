dcb1fba2755ed06811f14e1785420cbf07a58864
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-22 11:01:57 -0700

    Create panic.h

---
+++ libc/include/panic.h
@@ -0,0 +1,12 @@
+/******************************************************************************
+ * Panic header file                                                          *
+ * (c) Copyleft DamieFC                                                       *
+ ******************************************************************************/
+
+#ifdef PANIC_H
+#undef PANIC_H
+#define PANIC_H
+
+void panic(void);
+
+#endif PANIC_H
