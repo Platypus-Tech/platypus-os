c3b936c75c7553293240da29c75f0eb56975b182
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-22 11:08:33 -0700

    Panic!

---
+++ libc/panic/panic.c
@@ -0,0 +1,10 @@
+#include <panic.h>
+#include <log/log.h>
+
+void panic(void){
+    printf("PANIC!!!!\n");
+    printf("Something happened :(\n");
+    printf("No idea what, actually. Maybe the computer went on a bathroom break. Maybe you miswired something. Maybe the Grim Reaper killed the computer.\n");
+    printf("No way of knowing right now.\n");
+    printf("Help update this code so have better information, including error codes, and maybe so this can automatically heal!\n");
+}
