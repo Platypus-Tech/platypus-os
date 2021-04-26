5525f93b57979a1f5048cb309125314da75abbaa
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-26 12:03:57 -0700

    IDK IF THIS WORKS

--- kernel/memory/mset.c
+++ kernel/memory/mset.c
@@ -1,6 +1,8 @@
 #include <string.h>
 #include <stdio.h>
+#include <stddef.h>
 #include "../main.h"
 *void mset(){
-    //memset(mustMakeMain.cAPointer, idk yet, idk yet);
+    memset(kernel_main,NULL,1000); // IDK IF THIS WORKS
+    printf("Hopefully kernel_main got 1000 bytes of memory.");
 }
