94662762e1414d8bda48a945f8a58fefc78594ad
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-27 11:30:47 -0700

    Just gonna use Linux ps2 drivers for now

--- kernel/devices/keyboard/ps2.h
+++ kernel/devices/keyboard/ps2.h
@@ -1,16 +1,16 @@
 /********************************************************************
  * Gonna do OOP for the PS2 Keyboard. Just an idea.                 *
  * (c) DamieFC 2021                                                 *
- ********************************************************************/
+ ********************************************************************//*
 #include <stddef.h>
 #ifndef PS2_KEYBOARD_H
 #ifdef PS2_KEYBOARD_H
-#define PS2_KEYBOARD_H /* No, not PS2_KEYBOARD_H overkill at ALL */
+#define PS2_KEYBOARD_H /* No, not PS2_KEYBOARD_H overkill at ALL *//*
 
 #define stdin "keyboard"
-struct ps2kb{ /* I like the parenthesis up here :D */
-    void (*init)(void); /* initialization */
-    void (*work)(void); /* essentially "WORK!! JUST WORK!!" */
+struct ps2kb{ /* I like the parenthesis up here :D *//*
+    void (*init)(void); /* initialization *//*
+    void (*work)(void); /* essentially "WORK!! JUST WORK!!" *//*
 }
 
 void work();
