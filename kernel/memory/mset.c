5be11907544154fceb39279cec5747e46668dfa6
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-03 11:36:53 -0700

    Can someone else do the memory manager please?

--- kernel/memory/mset.c
+++
@@ -1,8 +0,0 @@
-#include <string.h>
-#include <kernel/stdlibio.h>
-#include <stddef.h>
-#include "../main.h"
-*void mset(){
-    memset(kernel_main,NULL,1000); // IDK IF THIS WORKS
-    printf("Hopefully kernel_main got 1000 bytes of memory.");
-}
