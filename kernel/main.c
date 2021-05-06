2339d747eafc36cb91ce433493e6a87c92d2f39c
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-06 11:43:48 -0700

    Fix functions

--- kernel/main.c
+++ kernel/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <time.h>
-#include <kernel/tty.h>
+#include <kernel/text.h>
 //#include "devices/keyboard/keyboard.h"
 
 void *kernel_main(void) {
@@ -15,13 +15,13 @@ void *kernel_main(void) {
         c = fgetc(fptr);
     };
     fclose(fptr);
-    terminal_writestring("The time is %d", time_t);
-    terminal_writestring("Welcome to Platypus OS!\n");
-    terminal_writestring("This is still in development, so there isn't anything to do...\n\n\n");
-    terminal_writestring("Yet.\n");
-    terminal_writestring("If you want to help, go to https://github.com/Platypus-Tech/new-platypus-os-drafts/ to help\n");
-    terminal_writestring("...\n");
-    terminal_writestring("Or to see the source code!");
+    writestring("The time is %d", time_t);
+    writestring("Welcome to Platypus OS!\n");
+    writestring("This is still in development, so there isn't anything to do...\n\n\n");
+    writestring("Yet.\n");
+    writestring("If you want to help, go to https://github.com/Platypus-Tech/new-platypus-os-drafts/ to help\n");
+    writestring("...\n");
+    writestring("Or to see the source code!");
     //ps2kb.work();
     /*return("Chicken butt");*/ /* Eh no chicken butt */
     return("Really serious message :D POO POO PEEPEE"); /* Yeah, REALLY serious */
