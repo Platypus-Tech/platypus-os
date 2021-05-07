6bbe1c5cd9b3ae92d1399a7efa4b95920961d624
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-07 16:11:49 -0700

    Update panic.c

--- libc/panic/panic.c
+++ libc/panic/panic.c
@@ -1,10 +1,7 @@
 #include <panic.h>
 #include <log/log.h>
 
-void kpanic(void){
-    printf("PANIC!!!!\n");
-    printf("Something happened :(\n");
-    printf("No idea what, actually. Maybe the computer went on a bathroom break. Maybe you miswired something. Maybe the Grim Reaper killed the computer. Maybe you messed up the QEMU image.\n");
-    printf("No way of knowing right now.\n");
-    printf("Help update this code so have better information, including error codes, and maybe so this can automatically heal!\n");
+void kpanic(const char panicmessage){
+    asm(cli);
+    printf("Kernel panic: %s", panicmessage);
 }
