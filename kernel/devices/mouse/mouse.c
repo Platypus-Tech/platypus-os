1c0c79fe44257ae7dbb6bd7175732ebae349689f
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-11 12:15:11 -0700

    Acknowledgment credits in ext description |
    Acknowledgment stuff from https://wiki.osdev.org/PS/2_Mouse

--- kernel/devices/mouse/mouse.c
+++ kernel/devices/mouse/mouse.c
@@ -1,4 +1,6 @@
 #include "mouse.h"
+#include <log/log.h>
+#include <asm/io.h>
 
 void mouse_driver(){
     mouse_drvr_platypusos.init_mouse();
@@ -11,4 +13,16 @@ void init_mouse(){
     int left_butt_pressed = 0; // I did that on purpose xD
     int middle_butt_pressed = 0;
     int right_butt_pressed = 0;
+    log(INFO, "Mouse initialized!");
+}
+
+void work_mouse(){
+    outb(0xD4, 0x64);                    // tell the controller to address the mouse
+    outb(0xF3, 0x60);                    // write the mouse command code to the controller's data port
+    while(!(inb(0x64) & 1) asm("pause"); // wait until we can read
+    ack = inb(0x60);                     // read back acknowledge. This should be 0xFA
+    outb(0xD4, 0x64);                    // tell the controller to address the mouse
+    outb(100, 0x60);                     // write the parameter to the controller's data port
+    while(!(inb(0x64) & 1) asm("pause"); // wait until we can read
+    ack = inb(0x60);                     // read back acknowledge. This should be 0xFA
 }
