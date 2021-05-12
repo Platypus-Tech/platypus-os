29a58b622ab6c1032026e582166dc4ddb72119de
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-12 11:36:43 -0700

    Update vga.h

--- kernel/arch/i386/vga.h
+++ kernel/arch/i386/vga.h
@@ -23,11 +23,11 @@ enum vga_color {
 };
 
 static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) {
-	return fg | bg << 4;
+    return fg | bg << 4;
 }
 
 static inline uint16_t vga_entry(unsigned char uc, uint8_t color) {
-	return (uint16_t) uc | (uint16_t) color << 8;
+    return (uint16_t) uc | (uint16_t) color << 8;
 }
 
 #endif /* ARCH_386_VGA_H */
