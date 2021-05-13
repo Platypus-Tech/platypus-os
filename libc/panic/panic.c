0258e2d01111a280d731f1ce9557033055724aef
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-13 14:17:55 -0700

    Update panic.c

--- libc/panic/panic.c
+++ libc/panic/panic.c
@@ -1,6 +1,9 @@
 #include <panic.h>
 #include <log/log.h>
+#include <kernel/pkernel.h>
 
 void kpanic(const char panicmessage){
+    cls();
     printf("Kernel panic: %s", panicmessage);
+    log(PANIC, "%s", panicmessage);
 }
