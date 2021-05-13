44e4ed473ddb939fb490fd6d301862b17a8dd3b5
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-13 09:56:44 -0700

    Create pkernel.h

---
+++ kernel/include/kernel/pkernel.h
@@ -0,0 +1,12 @@
+/************************************************************
+ * Kernel functions.                                        *
+ * (c) Copyleft DamieFC                                     *
+ ************************************************************/
+#ifdef PKERNEL_H
+#undef PKERNEL_H
+#define PKERNEL_H
+#else
+#ifndef PKERNEL_H
+#define PKERNEL_H
+
+#endif PKERNEL_H /* My hand hurts :( */
