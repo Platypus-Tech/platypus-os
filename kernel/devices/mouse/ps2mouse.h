2f8f49a23358614f06a7373babea5f5830502b46
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-13 11:21:24 -0700

    Update and rename mouse.h to ps2mouse.h

--- kernel/devices/mouse/ps2mouse.h
+++ kernel/devices/mouse/ps2mouse.h
@@ -2,14 +2,16 @@
  * IDK computer mouse driver                                  *
  * (c) Copyleft 2021 DamieFC                                  *
  **************************************************************/
-#ifdef MOUSE_H
-#undef MOUSE_H
-#define MOUSE_H
+#ifdef PS2MOUSE_H
+#undef PS2MOUSE_H
+#define PS2MOUSE_H
 #else
-#ifndef MOUSE_H
-#define MOUSE_H // Must be sure :D
+#ifndef PS2MOUSE_H
+#define PS2MOUSE_H // Must be sure :D
 
 struct mouse_drvr_platypusos{
     void init_mouse();
     void work_mouse();
 }
+
+#endif PS2MOUSE_H
