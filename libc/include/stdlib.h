5a54e14bc00fd9cc9c6faa1b2cacf3694470419f
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-13 08:11:16 -0700

    add system() function

--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -1,17 +1,18 @@
 #ifndef _STDLIB_H
 #define _STDLIB_H 1
- 
+
 #include <sys/cdefs.h>
- 
+
 #ifdef __cplusplus
 extern "C" {
 #endif
- 
+
 __attribute__((__noreturn__))
 void abort(void);
- 
+int	system(const char *);
+
 #ifdef __cplusplus
 }
 #endif
- 
+
 #endif
