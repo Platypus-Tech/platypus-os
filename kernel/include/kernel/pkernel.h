901f223ca11381829dd1680a713f243d8c757990
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-13 14:16:54 -0700

    cls (clear screen) function

--- kernel/include/kernel/pkernel.h
+++ kernel/include/kernel/pkernel.h
@@ -9,14 +9,18 @@
 #ifndef PKERNEL_H
 #define PKERNEL_H
 #include <kernel/text.h>
+#include <stdio.h>
 
 void print(const char *textthing);
 void print(const char *textthing){
     writestring("%s", textthing);
 }
+
 void cls(void);
 void cls(void){
-    
+    while (terminal_row != 0 && terminal_column != 0){
+        printf("\b");
+    }
 }
 
 #endif PKERNEL_H /* My hand hurts :( */
