9f0ea8e7d2163cfceb809cc2c94f12909c1b5a42
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-13 14:14:42 -0700

    Update text.c

--- kernel/arch/i386/text.c
+++ kernel/arch/i386/text.c
@@ -11,8 +11,6 @@ static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
 static uint16_t* const VGA_MEMORY = (uint16_t*) 0xB8000;
 
-static size_t terminal_row;
-static size_t terminal_column;
 static uint8_t terminal_color;
 static uint16_t* terminal_buffer;
 
