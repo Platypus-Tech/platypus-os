b2ab17dd3fc93237fa0a89a657563275872f2556
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-13 10:52:49 -0700

    add print function

--- kernel/include/kernel/pkernel.h
+++ kernel/include/kernel/pkernel.h
@@ -8,5 +8,11 @@
 #else
 #ifndef PKERNEL_H
 #define PKERNEL_H
+#include <kernel/text.h>
+
+void print(const char *textthing);
+void print(const char *textthing){
+    writestring("%s", textthing);
+}
 
 #endif PKERNEL_H /* My hand hurts :( */
