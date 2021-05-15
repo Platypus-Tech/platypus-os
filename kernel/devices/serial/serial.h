abf86fb4ba5c046ae73d4c55aecf1506801e7939
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-15 14:08:57 -0700

    Starting to work on serial drivers

---
+++ kernel/devices/serial/serial.h
@@ -0,0 +1,16 @@
+/******************************************************************************
+ * Serial driver for Platypus OS                                              *
+ * (c) DamieFC 2021                                                           *
+ ******************************************************************************/
+
+#ifdef SERIAL_H
+    #undef SERIAL_H
+    #define SERIAL_H
+#else
+    #ifndef SERIAL_H
+    #define SERIAL_H
+struct serial_drvr_platypusos{
+    void serial_init(void);
+    void serial_read(int port);
+    void serial_write(int value, port);
+}
