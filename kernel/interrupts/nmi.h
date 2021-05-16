3d8a96eafa6cd8dba94f7850f72416099fd57a48
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-05-16 20:52:18 +0530

    nmi.h : Updated

--- kernel/interrupts/nmi.h
+++ kernel/interrupts/nmi.h
@@ -1,7 +1,7 @@
 #ifndef _KERNEL_INTERRUPTS_NMI_H
 #define _KERNEL_INTERRUPTS_NMI_H
 
-void NMI_enable(void);
-void NMI_disable(void);
+void NMI_enable();
+void NMI_disable();
 
 #endif //_KERNEL_INTERRUPTS_NMI_H
