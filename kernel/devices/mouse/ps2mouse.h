a1491ab94648890cff6c323df00c7a21b7bbfc6b
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-05-17 20:10:45 +0530

    Update ps2mouse.h

--- kernel/devices/mouse/ps2mouse.h
+++ kernel/devices/mouse/ps2mouse.h
@@ -2,16 +2,10 @@
  * IDK computer mouse driver                                  *
  * (c) Copyleft 2021 DamieFC                                  *
  **************************************************************/
-#ifdef _PS2MOUSE_H
-#undef _PS2MOUSE_H
-#define _PS2MOUSE_H
-#else
 #ifndef _PS2MOUSE_H
 #define _PS2MOUSE_H // Must be sure :D
 
-struct mouse_drvr_platypusos {
-    void init_mouse();
-    void work_mouse();
-}
+void init_mouse();
+void work_mouse();
 
 #endif _PS2MOUSE_H
