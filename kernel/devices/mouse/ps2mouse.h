1b0dae13b08b22c43c0a73caed9ab09313680eb3
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-05-17 20:12:19 +0530

    Update ps2mouse.h

--- kernel/devices/mouse/ps2mouse.h
+++ kernel/devices/mouse/ps2mouse.h
@@ -5,6 +5,14 @@
 #ifndef _PS2MOUSE_H
 #define _PS2MOUSE_H // Must be sure :D
 
+struct mouse_driver {
+  int x_mouse_pos;
+  int y_mouse_pos;
+  int left_btn_pressed ;
+  int middle_btn_pressed;
+  int right_btn_pressed;
+}
+
 void init_mouse();
 void work_mouse();
 
