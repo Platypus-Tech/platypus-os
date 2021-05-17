a3273c4721cbc21e240f7ea6e5c6bdc5bbf02e38
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-17 08:58:13 -0700

    Moving those functions back to struct to test

--- kernel/devices/mouse/ps2mouse.h
+++ kernel/devices/mouse/ps2mouse.h
@@ -6,14 +6,13 @@
 #define _PS2MOUSE_H // Must be sure :D
 
 struct mouse_driver {
-  int x_mouse_pos;
-  int y_mouse_pos;
-  int left_btn_pressed;
-  int middle_btn_pressed;
-  int right_btn_pressed;
+    int x_mouse_pos;
+    int y_mouse_pos;
+    int left_btn_pressed;
+    int middle_btn_pressed;
+    int right_btn_pressed;
+    void init_mouse(void);
+    void work_mouse(void);
 }
-
-void init_mouse();
-void work_mouse();
 
 #endif _PS2MOUSE_H
