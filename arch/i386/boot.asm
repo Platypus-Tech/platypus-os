6d8063a1f38c3903bfe9bc46c3f86119d7ca71d6
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-05-19 17:50:26 +0530

    Added boot.asm

---
+++ arch/i386/boot.asm
@@ -0,0 +1,36 @@
+[BITS 32]
+
+global start
+start:
+      mov esp, system_stack
+      jmp stublet
+
+ALIGN 4
+multiboot:
+
+    PAGE_ALIGN   equ 1<<0
+    MEMORY_INFO  equ 1<<0
+    KLUDGE_AOUT  equ 1<<16
+    MAGIC        equ 0x1BADB002
+    FLAGS        equ PAGE_ALIGN | MEMORY_INFO | KLUDGE_AOUT
+    CHECKSUM     equ -(MAGIC + FLAGS)
+
+    EXTERN code, bss, end
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
+    
+    dd multiboot
+    dd code
+    dd bss
+    dd end
+    dd start
+
+stublet:
+     extern kernel_init
+     call kernel_init
+     jmp $
+
+SECTION .bss
+    resb 8192            
+system_stack:
