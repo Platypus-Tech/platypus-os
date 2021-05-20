7d6865e2642757f596855ed230cb476152d62332
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-05-20 22:17:15 +0530

    Update ports.c

--- kernel/drivers/ports/ports.c
+++ kernel/drivers/ports/ports.c
@@ -1,11 +1,11 @@
 #include <ports.h>
 #include <stdint.h>
 
-uint8_t port_byte_in(unsigned short port) {
+uint8_t inp(unsigned short port) {
       unsigned char c;
       __asm__ volatile("inb %1, %0" : "=a" (c) : "dN" (port));
 }
 
-void port_byte_out(unsigned short port, unsigned char data) {
+void outp(unsigned short port, unsigned char data) {
      __asm__ volatile("outb %1, %0" : : "dN" (port), "a" (data));
 } 
