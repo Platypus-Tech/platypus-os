f77851c948c0dd587f23a3c5cbfeb171568aacd9
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-05-23 11:25:49 +0530

    Added GDT

--- build.sh
+++ build.sh
@@ -5,10 +5,11 @@
 
 export PATH="./toolchain/compiler/bin:$PATH"
 
+# Compile the OS
 i686-elf-as ./kernel/arch/i386/boot.S -o boot.o
-
 i686-elf-gcc -I./kernel/include/ -c ./kernel/drivers/vga/vga.c -o vga.o
+i686-elf-gcc -I./kernel/drivers/ -I./kernel/include/ -I./kernel/cpu/ -c ./init/init.c -o init.o
+nasm -f elf32 ./kernel/cpu/load_gdt.asm -o load_gdt.o
+i686-elf-gcc -I./kernel/include/ -I./kernel/drivers/ -c ./kernel/cpu/gdt.c -o gdt.o
 
-i686-elf-gcc -I./kernel/drivers/vga/ -I./kernel/include/ -c ./init/init.c -o init.o
-
-i686-elf-gcc -T ./kernel/arch/i386/linker.ld -o PlatypusOS.bin -ffreestanding -O2 -nostdlib boot.o vga.o init.o
+i686-elf-gcc -T ./kernel/arch/i386/linker.ld -o PlatypusOS.bin -ffreestanding -O2 -nostdlib boot.o vga.o gdt.o load_gdt.o init.o
--- init/init.c
+++ init/init.c
@@ -1,8 +1,10 @@
-#include <vga.h>
+#include <vga/vga.h>
+#include <gdt.h>
 
 void kernel_init() {
     
      terminal_initialize();
 
      writestr("Welcome to PlatypusOS!");
+     init_gdt();
 }
