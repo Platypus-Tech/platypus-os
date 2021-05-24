d8f2b50a299323dd9b250848d7a2ba402249ef11
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-05-24 17:49:49 +0530

    Fixed Keyboard Driver

--- kernel/drivers/keyboard/keyboard.c
+++ kernel/drivers/keyboard/keyboard.c
@@ -5,14 +5,43 @@
 #include <vga/vga.h>
 
 uint8_t keyboard_layout[128] = {
-    0,   27,   '1',  '2', '3',  '4', '5', '6', '7', '8', '9', '0', '-',
-    '=', '\b', '\t', 'q', 'w',  'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-    '[', ']',  '\n', 0,   'a',  's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
-    ';', '\'', '`',  0,   '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',',
-    '.', '/',  0,    '*', 0,    ' ', 0,   0,   0,   0,   0,   0,   0,
-    0,   0,    0,    0,   0,    0,   0,   0,   0,   '-', 0,   0,   0,
-    '+', 0,    0,    0,   0,    0,   0,   0,   0,   0,   0,   0,
-};
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',
+  '9', '0', '-', '=', '\b',
+  '\t',			
+  'q', 'w', 'e', 'r',	
+  't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',
+    0,			
+  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	
+ '\'', '`',   0,		
+ '\\', 'z', 'x', 'c', 'v', 'b', 'n',			
+  'm', ',', '.', '/',   0,				
+  '*',
+    0,	
+  ' ',	
+    0,	
+    0,	
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,	
+    0,	
+    0,	
+    0,	
+    0,	
+    0,	
+  '-',
+    0,	
+    0,
+    0,	
+  '+',
+    0,	
+    0,	
+    0,	
+    0,	
+    0,
+    0,   0,   0,
+    0,	
+    0,	
+    0,	
+};		
 
 void handler_keyboard() {
   uint8_t keyboard_key_scancode;
@@ -23,8 +52,7 @@ void handler_keyboard() {
     // Shift, Ctrl keys to be implemented
 
   } else {
-    char input = keyboard_layout[keyboard_key_scancode];
-    terminal_putchar(input);
+    putch(keyboard_layout[keyboard_key_scancode]);
   }
 }
 
