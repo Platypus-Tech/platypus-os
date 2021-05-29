c634951f2f24bc29826839a868f4e09d479fd5b0
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-05-29 17:37:27 +0530

    Added VFS

---
+++ kernel/fs/vfs/vfs.c
@@ -0,0 +1,65 @@
+#include <stdint.h>
+#include <vga/vga.h>
+#include "vfs.h" 
+
+/* A simple VFS, based on JamesM's kernel development tutorials */
+
+uint32_t read_vfs(vfs_node_t *node, uint32_t offset, uint32_t size, uint8_t *buf) {
+    if (node->read != 0) {
+        return node->read(node, offset, size, buf);
+    }
+    else {
+       return 0;
+    }
+}
+
+uint32_t write_vfs(vfs_node_t *node, uint32_t offset, uint32_t size, uint8_t *buf) {
+    if (node->write != 0) {
+        return node->write(node, offset, size, buf);
+    }
+    else {
+        return 0;
+    }
+}
+
+void open_vfs(vfs_node_t *node, uint8_t read, uint8_t write) {
+    if (node->read != 0) {
+        return node->open(node);
+    }
+    else {
+        
+    }
+}
+
+void close_vfs(vfs_node_t *node) {
+    if (node->read != 0) {
+        return node->close(node);
+    }
+    else {
+        
+    }
+}
+
+struct dirent *readdir_vfs(vfs_node_t *node, uint32_t index) {
+    if ((node->flags & 0x7) == VFS_DIR && node->readdir != 0) {
+        return node->readdir(node, index);
+    }
+    else {
+        writestr("VFS: Not a directory!");
+        return 0;
+    }
+}
+
+vfs_node_t *finddir_vfs(vfs_node_t *node, char *name) {
+    if ((node->flags & 0x7) == VFS_DIR && node->readdir != 0) {
+        return node->finddir(node, name);
+    }
+    else {
+        writestr("VFS: no such directory");
+    }
+}
+
+void init_vfs() {
+    vfs_node_t *fs_root = 0;
+    writestr("[OK] Mount VFS");
+}
