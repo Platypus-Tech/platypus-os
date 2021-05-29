4d0b35bf1d3182e9db74e5761d2f7660ab9fa683
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-05-29 13:39:05 +0530

    Update memory.c

--- kernel/system/memory.c
+++ kernel/system/memory.c
@@ -1,16 +1,14 @@
 #include "memory.h"
-#include <vga/vga.h>
 #include <stdint.h>
 
 extern __kernel_end;
-
 unsigned int heap;
 
 void *malloc(unsigned int size) {
     uint32_t ptr = __kernel_end + heap;
     heap += size;
     
-    return (void*) ptr;
+    return (void*)ptr;
 }
 
 void free(void *addr) {
