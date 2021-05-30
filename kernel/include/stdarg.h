5c8ff34075cdc58c365e941183b1880c37001457
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-05-30 16:39:16 +0530

    Update stdarg.h

--- kernel/include/stdarg.h
+++ kernel/include/stdarg.h
@@ -6,6 +6,6 @@ typedef char va_list;
 #define va_arg(ap, type)    
 #define va_copy(dest, src) 
 #define va_end(ap)          
-#define va_start(ap, parmN)
+#define va_start(ap, p)
 
 #endif //_STDARG_H
