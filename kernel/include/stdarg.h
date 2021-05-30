e3e2a2b956a8655770a0f72ce4331cdd9bf70e67
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-05-30 16:36:52 +0530

    Create stdarg.h

---
+++ kernel/include/stdarg.h
@@ -0,0 +1,11 @@
+#ifndef _STDARG_H
+#define _STDARG_H
+
+typedef char va_list;
+
+#define va_arg(ap, type)    
+#define va_copy(dest, src) 
+#define va_end(ap)          
+#define va_start(ap, parmN)
+
+#endif //_STDARG_H
