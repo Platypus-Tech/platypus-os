1e393650bfe0ed8dffc903e499ee295dedc966c4
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-05-30 16:52:45 +0530

    Update memory.c

--- kernel/system/memory.c
+++ kernel/system/memory.c
@@ -4,11 +4,20 @@
 extern __kernel_end;
 unsigned int heap;
 
+static blk_hdr base;
+static blk_hdr *free_p = NULL;
+
 void *malloc(unsigned int size) {
-    uint32_t ptr = __kernel_end + heap;
-    heap += size;
+    blk_hdr *p, *prev_p;
+    blk_hdr *mcore(unsigned);
+    unsigned total_units;
     
-    return (void*)ptr;
+    total_units = (size + sizeof(blk_hdr) -1) / sizeof(blk_hdr) + 1;
+    
+    if ((prev_p = free_p) == NULL) {
+        base.s.ptr = free_p = prev_p = &base;
+        base.s.size = 0;
+    }
 }
 
 void free(void *addr) {
