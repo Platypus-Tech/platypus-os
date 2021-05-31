096145f8b1c0c0d5c47d986ae2cd179423f3522c
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-31 11:46:15 -0700

    Add another unusable function *yayayay*

--- kernel/drivers/pcspkr/pcspkr.c
+++ kernel/drivers/pcspkr/pcspkr.c
@@ -2,10 +2,14 @@
 #include "../keyboard/keyboard.h"
 #include "../pit/pit.h"
 
-void pcspkr_init() {
+void pcspkr_init(void) {
     
 }
 
 void beep(size_t frequency){
+    
+}
+
+void stop_sound(void){
     
 }
