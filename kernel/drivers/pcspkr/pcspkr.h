6a4efb33f9370d4ec38733c1b7f6d81213f89fe6
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-06-01 10:42:41 +0530

    Delete pcspkr.h

--- kernel/drivers/pcspkr/pcspkr.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DRIVERS_PCSPKR_PCSPKR_H
-#define _DRIVERS_PCSPKR_PCSPKR_H
-
-#include <stddef.h>
-#include <stddint.h> // Not sure if I need it
-
-void pcspkr_init(void);
-void beep(size_t frequency);
-void stop_sound(void);
