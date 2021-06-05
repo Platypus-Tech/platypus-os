dd366ebef73b8425b746e8f990ebeb3fa20b5f33
GNUOverYooouu <dtortoise99@gmail.com>
2021-06-05 09:41:01 -0700

    Startin' on terminal

---
+++ kernel/system/terminal.h
@@ -0,0 +1,14 @@
+/**********************************************************
+ * The terminal ported from KinuxOS with new code         *
+ * Ported code (c) DamieFC 2021 original code (c)         *
+ * kushagra765 under the Unlicense                        *
+ **********************************************************/
+
+#ifndef _TERMINAL_H
+#define _TERMINAL_H
+
+void putPrompt(void);
+void checkCmd(void);
+char *commands[];
+
+#endif _TERMINAL_H
