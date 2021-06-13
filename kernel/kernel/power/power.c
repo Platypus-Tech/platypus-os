296b579aaa7443884708b5797302dc846261c515
GNUOverYooouu <dtortoise99@gmail.com>
2021-06-13 11:18:04 -0700

    Moving reboot() definition to kernel/power/power.c

---
+++ kernel/kernel/power/power.c
@@ -0,0 +1,14 @@
+#include <kernel/power.h>
+#include <system/vtconsole.h>
+#include <stddef.h>
+#include <kernel/ports.h>
+
+void reboot(void) {
+  vtconsole_delete(vtc);
+  uint8_t t = 0x02;
+  while (t & 0x02) {
+    t = inp(0x64);
+  }
+  outp(0x64, 0xFE);
+  __asm__ volatile("hlt");
+}
