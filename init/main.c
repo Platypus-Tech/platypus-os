504e2e71d36f2370f0b4f485d25277cac7c143fc
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-06-15 15:10:04 +0530

    Updated init.c

--- init/main.c
+++ init/main.c
@@ -34,6 +34,7 @@ void kernel_main() {
   writestr("[OK] Load Drivers\n");
 
   __asm__ volatile("sti");
+
   info_log("System Loaded\n");
 
   vtc = vtconsole(VGA_SCREEN_WIDTH, VGA_SCREEN_HEIGHT, paint_callback,
