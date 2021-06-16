e7b50593e48c2a65f74f244152e21e84f2f93268
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-06-16 15:13:37 +0530

    Updated

--- kernel/kernel/memory.h
+++ kernel/kernel/memory.h
@@ -3,21 +3,7 @@
 
 #include <stdint.h>
 
-typedef long align;
-
-union block_header {
-  struct {
-    union block_header *ptr;
-    unsigned block_size;
-  } s;
-  align x;
-};
-
-typedef union block_header blk_hdr;
-
 void *kmalloc(uint32_t size);
 void kfree(void *addr);
-void *malloc(unsigned int size);
-static blk_hdr *mcore(unsigned units);
 
 #endif //_SYSTEM_MEMORY_H
