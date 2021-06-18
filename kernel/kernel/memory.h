59ca8674c7b5a29298713e2532796c7568920113
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-06-18 15:22:21 +0530

    Updated Header files

--- kernel/kernel/memory.h
+++ kernel/kernel/memory.h
@@ -1,9 +1,9 @@
-#ifndef _SYSTEM_MEMORY_H
-#define _SYSTEM_MEMORY_H
+#ifndef _KERNEL_MEMORY_H
+#define _KERNEL_MEMORY_H
 
 #include <stdint.h>
 
 void *kmalloc(uint32_t size);
 void kfree(void *addr);
 
-#endif //_SYSTEM_MEMORY_H
+#endif //_KERNEL_MEMORY_H
