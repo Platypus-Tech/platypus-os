2b38f3748dd2f3fcd03a914de4a70af729e235a1
GNUOverYooouu <dtortoise99@gmail.com>
2021-08-21 12:28:22 -0700

    RTC header file Real Time Clock, not Regional Transportation Commission of Southern Nevada (Vegas moment)
    why not, framebuffer drawing's coming up, plus I've been itching to do some code.

---
+++ kernel/drivers/rtc/rtc.h
@@ -0,0 +1,8 @@
+/* Real Time Clock, not Regional Transportation Commission of Southern Nevada (Vegas moment) */
+
+#ifndef _DRIVERS_RTC_H
+#define _DRIVERS_RTC_H
+
+void init_rtc();
+
+#endif _DRIVERS_RTC_H
