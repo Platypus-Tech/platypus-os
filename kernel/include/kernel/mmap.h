6c44b5a585cc209b5c7284e1d9ca02b8cab77d18
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-10-19 17:26:10 +0530

    mm: removed mmap

--- kernel/include/kernel/mmap.h
+++
@@ -1,9 +0,0 @@
-#ifndef _KERNEL_MMAP_H
-#define _KERNEL_MMAP_H
-
-#include <init/multiboot.h>
-
-void verify_mmap(multiboot_info_t *mboot_info);
-void print_mmap(multiboot_info_t *mboot_info);
-
-#endif //_KERNEL_MMAP_H
