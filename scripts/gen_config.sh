16148c0dc547177f97f04bde691cbaa043d1c5d8
kushagra <74482007+kushagra765@users.noreply.github.com>
2021-12-07 17:58:03 +0530

    Updated

--- scripts/gen_config.sh
+++ scripts/gen_config.sh
@@ -12,4 +12,4 @@ if [ -f kernel/include/generated/config.h ]; then
    rm kernel/include/generated/config.h
 fi
 
-echo "#ifndef _GENERATED_CONFIG_H\n#define _GENERATED_CONFIG_H\n\n#define CONFIG_ARCH \"$CONFIG_ARCH\"\n#define CONFIG_USERNAME \"$CONFIG_USERNAME\"\n#define CONFIG_USERMODE '$CONFIG_USERMODE'\n\n#endif //_GENERATED_CONFIG_H" >> kernel/include/generated/config.h
+echo "#ifndef _GENERATED_CONFIG_H\n#define _GENERATED_CONFIG_H\n\n#define CONFIG_ARCH \"$CONFIG_ARCH\"\n#define CONFIG_USERNAME \"$CONFIG_USERNAME\"\n#define CONFIG_USERMODE '$CONFIG_USERMODE'\n\n#endif //_GENERATED_CONFIG_H" > kernel/include/generated/config.h
