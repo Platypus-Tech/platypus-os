e283b1415c859651175c7dcf0fbb4cdf6680e5b7
kushagra <74482007+kushagra765@users.noreply.github.com>
2022-03-01 18:18:03 +0530

    device manager: updated

--- kernel/include/kernel/device.h
+++ kernel/include/kernel/device.h
@@ -5,12 +5,14 @@
 
 struct device {
   const char *name;
+  const char *path;
   uint32_t id;
 };
 
 void add_device(struct device *dev);
 int get_device_by_id(int id);
 int remove_device_by_id(int id);
+int print_devices();
 void init_device_manager();
 
 #endif //_KERNEL_DEVICE_H
